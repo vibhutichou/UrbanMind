095e403ab9b78121747defa1420d9a8440a540a3
vibhutichou <vibhutichoudhary0812@gmail.com>
2026-02-06 20:45:46 +0530

    Remove old Urbanmind Microservices directory

--- Urbanmind Microservices/urbanmind-auth/src/main/java/com/urbanmind/urbanmind_auth/dto/request/VerifyOtpRequest.java
+++
@@ -1,7 +0,0 @@
-//package com.urbanmind.urbanmind_auth.dto.request;
-//
-//public class VerifyOtpRequest {
-//    private String email;
-//    private String otp;
-//}
-//
--- Urbanmind Microservices/urbanmind-auth/src/main/java/com/urbanmind/urbanmind_auth/repository/RevokedTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.urbanmind.urbanmind_auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.urbanmind.urbanmind_auth.entity.RevokedToken;
-
-public interface RevokedTokenRepository extends JpaRepository<RevokedToken, Long> {
-    boolean existsByToken(String token);
-}
